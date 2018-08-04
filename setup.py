ac8584abc808727036170a2a36b2f52aa51e9167
tundebabzy <tundebabzy@gmail.com>
2018-08-04 01:14:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 	version=version,
 	description='Send notifications ',
 	author='XLevel Retail Systems Nigeria Ltd',
-	author_email='olamide@xlevelretail.com',
+	author_email='tunde@francisakindele.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
